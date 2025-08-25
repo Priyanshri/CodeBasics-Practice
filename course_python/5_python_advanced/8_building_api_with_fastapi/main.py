d60ce21afd31026ce820f2b34f3d10f37f082aa3
Priyanshri <priyanshripriyadarshinipanda30@gmail.com>
2025-08-25 11:52:29 +0530

    learned little about fastapi

--- course_python/5_python_advanced/8_building_api_with_fastapi/main.py
+++ course_python/5_python_advanced/8_building_api_with_fastapi/main.py
@@ -1,3 +1,6 @@
+# uninstall previous version of fastapi if any by : pip uninstall fastapi or, pip uninstall uvicorn
+# install fastapi by : pip install "fastapi[standard]" 
+
 from fastapi import FastAPI
 from enum import Enum
 
@@ -27,3 +30,11 @@ coupon_code = {
 @app.get("/get_coupon/{code}")
 async def get_items(code: int):
     return { 'discount_amount': coupon_code.get(code) }
+
+
+# to run this code got to the previous folder by : cd C:\Code\course_python\5_python_advanced\8_building_api_with_fastapi
+#  then run : fastapi dev .\main.py or, python -m uvicorn main:app
+# then open the browser by clicking the link which appears like :  http://127.0.0.1:8000
+# we can see documentation by going to :  http://127.0.0.1:8000/docs
+
+# You can see all the details of fastapi in : https://fastapi.tiangolo.com/
